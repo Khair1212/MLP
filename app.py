cb9cff3773e05e662ad70861190d4ff3ab6f5156
Khair Ahammed <khairahmad6@gmail.com>
2022-12-24 00:39:34 +0600

    testing deployment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "CI CD pipeline has been established."
+    return "CI CD pipeline has been established. Hello There"
 
 
 if __name__=="__main__":
