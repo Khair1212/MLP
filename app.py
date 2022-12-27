ddced93dd1804394e09b05d7c29b4c8bf470833c
Khair Ahammed <khairahmad6@gmail.com>
2022-12-27 23:06:58 +0600

    Logger and Exception Handler added

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
-
+import sys
+from housing import exception, logger
 app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("we are trying to ")
+    except Exception as e:
+        housing = exception.HousingException(e, sys)
+        logger.logging.info(housing.error_message)
+        logger.logging.info("adfadfa")
+
     return "CI CD pipeline has been established. Hello There"
 
 
